fa13493b1b5b9ba555ee6c2d4843afb2d2a7ebcc
Eurico77 <euricomagalhaes61@gmail.com>
2020-06-15 16:50:16 -0300

    finish

---
+++ desafio_02/desafio.js
@@ -0,0 +1,17 @@
+//Desafios para fortalecer alguns conceitos, entre eles:
+//Objetos;
+//Vetores.
+
+const empresa = {
+  Nome: " Rocketseat",
+  Cor: "Roxo",
+  Foco: "Programação",
+  Endereco: {
+    Rua: "Rua Guilherme Gembala",
+    Número: "260",
+  },
+};
+
+console.log(
+  ` a empresa ${empresa.Nome} esta localizada em ${empresa.Endereco.Rua}, ${empresa.Endereco.Número}`
+);
