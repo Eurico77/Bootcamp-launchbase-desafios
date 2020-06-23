a0384d5d5297900ba559333c268c45d5a9460768
Eurico77 <euricomagalhaes61@gmail.com>
2020-06-23 11:20:46 -0300

    ok

--- desafio_03/desafio.js
+++ desafio_03/desafio.js
@@ -1,9 +1,7 @@
 /*==========================================
 Crie um programa que armazena um array de usuários (objetos),
  cada usuário tem um nome e suas tecnologias
-
-
-============================================*/
+*/
 
 const usuarios = [
   { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
@@ -11,29 +9,15 @@ const usuarios = [
   { nome: "Tuane", tecnologias: ["HTML", "Node.js"] },
 ];
 
-function mostraTechs(user) {
-  for (let i = 0; i < user.length; i++) {
-    console.log(`o ${user[i].nome} trabalha com ${user[i].tecnologias}`);
-  }
+for( usuario of usuarios ) {
+
+  console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(", ")}`)
 }
 
-////////////////////////////////////////////////
 
-// Busca por tecnologia
-function checaSeUsuarioUsaCSS(usuario) {
-  for (let tecnologia of usuario.tecnologias) {
-    if (tecnologia == "CSS") return true;
-  }
-  return false;
-}
 
-for (let i = 0; i < usuarios.length; i++) {
-  const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
 
-  if (usuarioTrabalhaComCSS) {
-    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
-  }
-}
 
-//mostraTechs(usuarios);
-checaSeUsuarioUsaCSS(usuarios);
+
+//mostraTechs(usuarios)
+
